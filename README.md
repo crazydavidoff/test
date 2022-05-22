29a39fb5eaea2d8ef460da3a93137867200df68e
Artem Davydov <artem.davydov@altenar.com>
2022-05-22 19:01:27 +0300

    1 commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # test
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_google"></a> [google](#provider\_google) | 4.21.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [google_compute_firewall.http-server](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall) | resource |
+| [google_compute_instance.default](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance) | resource |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_ip"></a> [ip](#output\_ip) | n/a |
+<!-- END_TF_DOCS -->
---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "google_compute_instance" "default" {
+  name         = "myvirtmachine"
+  machine_type = "f1-micro"
+  zone         = "europe-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+    metadata_startup_script = "${file("startup.sh")}"
+    tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http-terraform"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+}
