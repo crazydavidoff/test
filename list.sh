550249640e8c298b6761ad6d323808d49c705699
Artem Davydov <artem.davydov@altenar.com>
2022-06-05 19:22:06 +0300

    fix

--- list.sh
+++ list.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for dir in $(ls -R | grep ":$" | sed -e 's/:$//')
+for dir in $(find . -type dir | tail -n +2)
 do
-  ls -p $dir | grep -v / > $dir/list
+cat <<EOF > $dir/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+EOF
+find -E $dir -regex ".*\.(yml|yaml)" ! -name kustomization.yaml -type file -exec basename {} \; | sed -e 's/^/- /' >> $dir/kustomization.yaml
 done
